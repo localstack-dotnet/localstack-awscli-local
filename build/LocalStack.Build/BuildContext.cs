453174893ea42f9c2a7a186ebf8ad4b147cd58bb
deniz_irgin@yahoo.com <deniz_irgin@yahoo.com>
2023-02-01 20:43:58 +0300

    replace azure devops with github actions

---
+++ build/LocalStack.Build/BuildContext.cs
@@ -0,0 +1,194 @@
+﻿namespace LocalStack.Build;
+
+public sealed class BuildContext : FrostingContext
+{
+    public BuildContext(ICakeContext context) : base(context)
+    {
+        BuildConfiguration = context.Argument("config", "Release");
+        ForceBuild = context.Argument("force-build", false);
+        ForceRestore = context.Argument("force-restore", false);
+        PackageVersion = context.Argument("package-version", "x.x.x");
+        PackageId = context.Argument("package-id", default(string));
+        PackageSecret = context.Argument("package-secret", default(string));
+        PackageSource = context.Argument("package-source", default(string));
+
+        var sourceBuilder = ImmutableDictionary.CreateBuilder<string, string>();
+        sourceBuilder.AddRange(new[]
+        {
+            new KeyValuePair<string, string>("myget", "https://www.myget.org/F/localstack-dotnet-client/api/v3/index.json"),
+            new KeyValuePair<string, string>("nuget", "https://api.nuget.org/v3/index.json")
+        });
+        PackageSourceMap = sourceBuilder.ToImmutable();
+
+        SolutionRoot = context.Directory("../../");
+        SrcPath = SolutionRoot + context.Directory("src");
+        TestsPath = SolutionRoot + context.Directory("tests");
+        BuildPath = SolutionRoot + context.Directory("build");
+        ArtifactOutput = SolutionRoot + context.Directory("artifacts");
+        LocalStackAwsLocalFolder = SrcPath + context.Directory("LocalStack.AwsLocal");
+        SlnFilePath = SolutionRoot + context.File("LocalStack.sln");
+        LocalStackAwsLocalProjFile = LocalStackAwsLocalFolder + context.File("LocalStack.AwsLocal.csproj");
+
+        var packIdBuilder = ImmutableDictionary.CreateBuilder<string, FilePath>();
+        packIdBuilder.AddRange(new[]
+        {
+            new KeyValuePair<string, FilePath>("LocalStack.AwsLocal", LocalStackAwsLocalProjFile),
+        });
+        PackageIdProjMap = packIdBuilder.ToImmutable();
+    }
+
+    public string BuildConfiguration { get; }
+
+    public bool ForceBuild { get; }
+
+    public bool ForceRestore { get; }
+
+    public string PackageVersion { get; }
+
+    public string PackageId { get; }
+
+    public string PackageSecret { get; }
+
+    public string PackageSource { get; }
+
+    public ImmutableDictionary<string, string> PackageSourceMap { get; }
+
+    public ImmutableDictionary<string, FilePath> PackageIdProjMap { get; }
+
+    public ConvertableFilePath SlnFilePath { get; }
+
+    public ConvertableDirectoryPath SolutionRoot { get; }
+
+    public ConvertableDirectoryPath SrcPath { get; }
+
+    public ConvertableDirectoryPath TestsPath { get; }
+
+    public ConvertableDirectoryPath BuildPath { get; }
+
+    public ConvertableDirectoryPath ArtifactOutput { get; }
+
+    public ConvertableDirectoryPath LocalStackAwsLocalFolder { get; }
+
+    public ConvertableFilePath LocalStackAwsLocalProjFile { get; }
+
+    public static void ValidateArgument(string argumentName, string argument)
+    {
+        if (string.IsNullOrWhiteSpace(argument))
+        {
+            throw new Exception($"{argumentName} can not be null or empty");
+        }
+    }
+
+    public void InstallXUnitNugetPackage()
+    {
+        if (!Directory.Exists("testrunner"))
+        {
+            Directory.CreateDirectory("testrunner");
+        }
+
+        var nugetInstallSettings = new NuGetInstallSettings
+        {
+            Version = "2.4.1",
+            Verbosity = NuGetVerbosity.Normal,
+            OutputDirectory = "testrunner",
+            WorkingDirectory = "."
+        };
+
+        this.NuGetInstall("xunit.runner.console", nugetInstallSettings);
+    }
+
+    public IEnumerable<ProjMetadata> GetProjMetadata()
+    {
+        DirectoryPath testsRoot = this.Directory(TestsPath);
+        List<FilePath> csProjFile = this.GetFiles($"{testsRoot}/**/*.csproj").Where(fp => fp.FullPath.EndsWith("Tests.csproj")).ToList();
+
+        IList<ProjMetadata> projMetadata = new List<ProjMetadata>();
+
+        foreach (FilePath csProj in csProjFile)
+        {
+            string csProjPath = csProj.FullPath;
+
+            IEnumerable<string> targetFrameworks = GetProjectTargetFrameworks(csProjPath);
+            string directoryPath = csProj.GetDirectory().FullPath;
+            string assemblyName = GetAssemblyName(csProjPath);
+
+            var testProjMetadata = new ProjMetadata(directoryPath, csProjPath, targetFrameworks, assemblyName);
+            projMetadata.Add(testProjMetadata);
+        }
+
+        return projMetadata;
+    }
+
+    public void RunXUnitUsingMono(string targetFramework, string assemblyPath)
+    {
+        int exitCode = this.StartProcess("mono", new ProcessSettings
+        {
+            Arguments = $"./testrunner/xunit.runner.console.2.4.1/tools/{targetFramework}/xunit.console.exe {assemblyPath}"
+        });
+
+        if (exitCode != 0)
+        {
+            throw new InvalidOperationException($"Exit code: {exitCode}");
+        }
+    }
+
+    public string GetProjectVersion()
+    {
+        FilePath file = this.File("./src/Directory.Build.props");
+
+        this.Information(file.FullPath);
+
+        string project = File.ReadAllText(file.FullPath, Encoding.UTF8);
+        int startIndex = project.IndexOf("<Version>", StringComparison.Ordinal) + "<Version>".Length;
+        int endIndex = project.IndexOf("</Version>", startIndex, StringComparison.Ordinal);
+
+        string version = project.Substring(startIndex, endIndex - startIndex);
+        version = $"{version}.{PackageVersion}";
+
+        return version;
+    }
+
+    private IEnumerable<string> GetProjectTargetFrameworks(string csprojPath)
+    {
+        FilePath file = this.File(csprojPath);
+        string project = File.ReadAllText(file.FullPath, Encoding.UTF8);
+
+        bool multipleFrameworks = project.Contains("<TargetFrameworks>");
+        string startElement = multipleFrameworks ? "<TargetFrameworks>" : "<TargetFramework>";
+        string endElement = multipleFrameworks ? "</TargetFrameworks>" : "</TargetFramework>";
+
+        int startIndex = project.IndexOf(startElement, StringComparison.Ordinal) + startElement.Length;
+        int endIndex = project.IndexOf(endElement, startIndex, StringComparison.Ordinal);
+
+        string targetFrameworks = project.Substring(startIndex, endIndex - startIndex);
+
+        return targetFrameworks.Split(';');
+    }
+
+    private string GetAssemblyName(string csprojPath)
+    {
+        FilePath file = this.File(csprojPath);
+        string project = File.ReadAllText(file.FullPath, Encoding.UTF8);
+
+        bool assemblyNameElementExists = project.Contains("<AssemblyName>");
+
+        string assemblyName;
+
+        if (assemblyNameElementExists)
+        {
+            int startIndex = project.IndexOf("<AssemblyName>", StringComparison.Ordinal) + "<AssemblyName>".Length;
+            int endIndex = project.IndexOf("</AssemblyName>", startIndex, StringComparison.Ordinal);
+
+            assemblyName = project.Substring(startIndex, endIndex - startIndex);
+        }
+        else
+        {
+            int startIndex = csprojPath.LastIndexOf("/", StringComparison.Ordinal) + 1;
+            int endIndex = csprojPath.IndexOf(".csproj", startIndex, StringComparison.Ordinal);
+
+            assemblyName = csprojPath.Substring(startIndex, endIndex - startIndex);
+        }
+
+        return assemblyName;
+    }
+}
---
+++ build/LocalStack.Build/GlobalUsings.cs
@@ -0,0 +1,22 @@
+﻿global using Cake.Common;
+global using Cake.Common.Diagnostics;
+global using Cake.Common.IO;
+global using Cake.Common.IO.Paths;
+global using Cake.Common.Tools.DotNet.MSBuild;
+global using Cake.Common.Tools.NuGet;
+global using Cake.Common.Tools.NuGet.Install;
+global using Cake.Common.Tools.NuGet.List;
+global using Cake.Core;
+global using Cake.Core.IO;
+global using Cake.Frosting;
+
+global using LocalStack.Build;
+global using LocalStack.Build.Models;
+
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.Immutable;
+global using System.IO;
+global using System.Linq;
+global using System.Text;
+global using System.Text.RegularExpressions;
